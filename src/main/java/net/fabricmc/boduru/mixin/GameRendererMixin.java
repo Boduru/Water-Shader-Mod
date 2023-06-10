721379498a9d1408f9f0435a5f5a0d25a17a6555
jim <jimpavan@gmail.com>
2023-06-10 10:53:22 +0200

    Look up-down solved! WIP: - Jump (change Y problem) - Walk problem (change roll or yaw) - Hard Edges - Distortions

--- src/main/java/net/fabricmc/boduru/mixin/GameRendererMixin.java
+++ src/main/java/net/fabricmc/boduru/mixin/GameRendererMixin.java
@@ -21,55 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class GameRendererMixin {
     @Shadow @Final private Camera camera;
     @Shadow private boolean renderHand;
-
-//    @Inject(at = @At("HEAD"), method = "render")
-//    private void renderHead(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
-//        if (!WaterShaderMod.renderPass.doDrawWater()) {
-//            if (camera != null) {
-//                Vec3d position = camera.getPos();
-//                float pitch = camera.getPitch();
-//
-//                WaterShaderMod.cameraSav.pitch = pitch;
-//                WaterShaderMod.cameraSav.position = position;
-//                WaterShaderMod.cameraSav.cameraY = ((CameraMixin) camera).getCameraY();
-//                WaterShaderMod.cameraSav.lastCameraY = ((CameraMixin) camera).getLastCameraY();
-//
-//                double d = 2 * (position.getY() - WaterShaderMod.clipPlane.getHeight());
-//                ((CameraMixin) camera).setPitch(-pitch);
-//                ((CameraMixin)camera).invokeSetPos(position.getX(), position.getY() - d, position.getZ());
-//                ((CameraMixin) camera).invokeSetRotation(-pitch, ((CameraMixin) camera).getYaw());
-//            }
-//        }
-//        else {
-//            Vec3d position = WaterShaderMod.cameraSav.position;
-//            float pitch = WaterShaderMod.cameraSav.pitch;
-//            float cameraY = WaterShaderMod.cameraSav.cameraY;
-//            float lastCameraY = WaterShaderMod.cameraSav.lastCameraY;
-//
-//            if (camera != null) {
-//                ((CameraMixin) camera).setPitch(pitch);
-//                ((CameraMixin) camera).invokeSetRotation(pitch, ((CameraMixin) camera).getYaw());
-//                ((CameraMixin)camera).invokeSetPos(position.getX(), position.getY(), position.getZ());
-//            }
-//        }
-//    }
-
     @Shadow @Final private MinecraftClient client;
-
-//        @Inject(method = "renderWorld", at = @At(
-//            value = "INVOKE",
-//            // Inject before the call to Camera.update()
-//            target = "Lnet/minecraft/client/render/Camera;update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V",
-//            shift = At.Shift.BEFORE
-//        ))
-        private void transformationDown(float tickDelta, long limitTime, MatrixStack matrices, CallbackInfo ci) {
-            if (client.player == null) return;
-            if (client.player.getPos() == null) return;
-
-            double d = 2 * (client.player.getPos().getY() - WaterShaderMod.clipPlane.getHeight());
-//            matrices.multiplyPositionMatrix(new Matrix4f().translate(0, (float) -d, 0));
-//            matrices.translate(0, -d, 0);
-        }
 
     @Inject(at = @At("HEAD"), method = "renderWorld")
     private void PostCameraUpdate(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci) {
@@ -84,57 +36,22 @@ public abstract class GameRendererMixin {
                 WaterShaderMod.cameraSav.playerPitch = client.player.getPitch();
                 WaterShaderMod.cameraSav.playerPosition = client.player.getPos();
 
-                float pitch = -camera.getPitch();
-                double d = 2 * (client.player.getPos().getY() - WaterShaderMod.clipPlane.getHeight());
+                float pitch = -client.player.getPitch();
 
-//                client.player.setPos(client.player.getPos().getX(), client.player.getPos().getY() - d, client.player.getPos().getZ());
                 client.player.setPitch(pitch);
-                //client.player.setPos(client.player.getPos().getX(), client.player.getPos().getY() - d, client.player.getPos().getZ());
 
                 // Do not render hand
                 renderHand = false;
 
+                double d = 2 * (client.player.getPos().getY() - WaterShaderMod.clipPlane.getHeight());
                 WaterShaderMod.vanillaShaders.setupVanillaShadersModelMatrices(client, 0, (float) d, 0);
-
-                // Move camera as well
-                Vec3d cameraPos = camera.getPos();
-                ((CameraMixin) camera).setPitch(pitch);
-                ((CameraMixin) camera).invokeSetRotation(pitch, ((CameraMixin) camera).getYaw());
-//                ((CameraMixin) camera).invokeSetPos(cameraPos.getX(), cameraPos.getY(), cameraPos.getZ());
+                System.out.println(pitch);
+                System.out.println(camera.getPitch());
             }
         } else {
             renderHand = true;
             WaterShaderMod.vanillaShaders.setupVanillaShadersModelMatrices(client, 0, 0, 0);
         }
-
-//        if (!WaterShaderMod.renderPass.doDrawWater()) {
-//            if (camera != null) {
-//                Vec3d position = camera.getPos();
-//                float pitch = camera.getPitch();
-//
-//                WaterShaderMod.cameraSav.pitch = pitch;
-//                WaterShaderMod.cameraSav.position = position;
-//                WaterShaderMod.cameraSav.cameraY = ((CameraMixin) camera).getCameraY();
-//                WaterShaderMod.cameraSav.lastCameraY = ((CameraMixin) camera).getLastCameraY();
-//
-//                double d = 2 * (position.getY() - WaterShaderMod.clipPlane.getHeight());
-//                ((CameraMixin) camera).setPitch(-pitch);
-//                ((CameraMixin)camera).invokeSetPos(position.getX(), position.getY() - d, position.getZ());
-//                ((CameraMixin) camera).invokeSetRotation(-pitch, ((CameraMixin) camera).getYaw());
-//            }
-//        }
-//        else {
-//            Vec3d position = WaterShaderMod.cameraSav.position;
-//            float pitch = WaterShaderMod.cameraSav.pitch;
-//            float cameraY = WaterShaderMod.cameraSav.cameraY;
-//            float lastCameraY = WaterShaderMod.cameraSav.lastCameraY;
-//
-//            if (camera != null) {
-//                ((CameraMixin) camera).setPitch(pitch);
-//                ((CameraMixin) camera).invokeSetRotation(pitch, ((CameraMixin) camera).getYaw());
-//                ((CameraMixin)camera).invokeSetPos(position.getX(), position.getY(), position.getZ());
-//            }
-//        }
     }
 
     @Inject(at = @At("TAIL"), method = "render")
@@ -149,18 +66,7 @@ public abstract class GameRendererMixin {
 
             if (camera.getFocusedEntity() != null && client.player != null && WaterShaderMod.cameraSav.playerPosition != null) {
                 float pitch = WaterShaderMod.cameraSav.playerPitch;
-                Vec3d position = WaterShaderMod.cameraSav.playerPosition;
-//                client.player.setPos(position.getX(), position.getY(), position.getZ());
                 client.player.setPitch(pitch);
-                //client.player.setPos(position.getX(), position.getY(), position.getZ());
-
-                // Move camera as well
-                Vec3d cameraPosition = WaterShaderMod.cameraSav.cameraPosition;
-                float cameraPitch = WaterShaderMod.cameraSav.cameraPitch;
-
-                ((CameraMixin) camera).setPitch(cameraPitch);
-                ((CameraMixin) camera).invokeSetRotation(cameraPitch, ((CameraMixin) camera).getYaw());
-//                ((CameraMixin) camera).invokeSetPos(cameraPosition.getX(), cameraPosition.getY(), cameraPosition.getZ());
             }
 
             gameRenderer.render(tickDelta, startTime, tick);
